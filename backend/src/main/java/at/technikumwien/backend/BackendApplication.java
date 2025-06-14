5ca5e86c00d85b7bbc714daba942dc5d9ef7e268
Michael <michael@langeder.org>
2025-06-14 12:53:48 +0200

    connect backend to postgres DB

--- backend/src/main/java/at/technikumwien/backend/BackendApplication.java
+++ backend/src/main/java/at/technikumwien/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package at.technikumwien.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "at.technikumwien.database.repository")
+@EntityScan(basePackages = "at.technikumwien.database.entity")
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
